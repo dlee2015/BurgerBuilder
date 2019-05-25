50c9b3d3c4bb9967f8537d6e0b79bf9abbb9b143
dlee2015 <dlee@Daniels-MacBook-Air.local>
2019-05-24 22:23:12 -0700

    implementation of redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -124,7 +125,7 @@ class ContactData extends Component {
 			].value;
 		}
 		const order = {
-			ingredients: this.props.ingredients,
+			ingredients: this.props.ings,
 			price: this.props.price,
 			order: formData
 		};
@@ -198,4 +199,14 @@ class ContactData extends Component {
 	}
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+	return {
+		ings: state.ingredients,
+		price: state.totalPrice
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	null
+)(ContactData);
